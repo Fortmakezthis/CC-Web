ca96d031999ddc509be2a845f0e4a5b44e535db0
Fortmakezthis <angela.sahai73@gmail.com>
2026-05-04 18:30:42 -0400

    Create serverInstaller.lua to install server files
    This script installs the server by downloading necessary files and setting up the startup script.

---
+++ serverInstaller.lua
@@ -0,0 +1,13 @@
+shell.run("wget https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/wubDater.lua /wubDater.lua")
+shell.run("wget https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/web/webUtils.lua /web/webUtils.lua")
+shell.run("wget https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/server/server.lua /web/server.lua")
+shell.run("wget https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/server/defaultIndex.lua /server/index.lua")
+
+local web = require("/web/webUtils")
+
+web.writeFile("/startup.lua", [[
+print("Starting server...")
+shell.run("/web/server.lua")
+]])
+
+print("Server installed! Please restart the computer to start the server.")
