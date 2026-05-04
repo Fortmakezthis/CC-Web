0f4011a701b57221a53ef8d86c995884347d4c00
Fortmakezthis <angela.sahai73@gmail.com>
2026-05-04 19:01:11 -0400

    Change server.lua download path in installer

--- serverInstaller.lua
+++ serverInstaller.lua
@@ -1,6 +1,6 @@
 shell.run("wget https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/wubDater.lua /wubDater.lua")
 shell.run("wget https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/web/webUtils.lua /web/webUtils.lua")
-shell.run("wget https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/server/server.lua /web/server.lua")
+shell.run("wget https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/server/server.lua /server/server.lua")
 shell.run("wget https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/server/defaultIndex.lua /server/index.lua")
 
 local web = require("/web/webUtils")
