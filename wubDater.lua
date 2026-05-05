84262f41be161f16ba3d20d3b9e188df97eca9c3
Fortmakezthis <angela.sahai73@gmail.com>
2026-05-05 16:50:25 -0400

    updated the wubdater to update mite

--- wubDater.lua
+++ wubDater.lua
@@ -1,10 +1,12 @@
 local webUtilsNEW = http.get("https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/web/webUtils.lua").readAll()
 local webNEW = http.get("https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/web.lua").readAll()
 local pageUtilsNEW = http.get("https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/web/pageUtils.lua").readAll()
+local miteNEW = http.get("https://raw.githubusercontent.com/Fortmakezthis/CC-Web/refs/heads/main/web/mite.lua").readAll()
 local webUtils = fs.open("/web/webUtils.lua", "r")
 local web = fs.open("/web.lua", "r")
 local pageUtils = fs.open("/web/pageUtils.lua")
-local webUtilsContent, webContent, pageUtilsContent
+local mite = fs.open("/web/mite.lua")
+local webUtilsContent, webContent, pageUtilsContent, miteContent
 if webUtils then
     webUtilsContent = webUtils.readAll()
     webUtils.close()
@@ -31,6 +33,14 @@ else
     pageUtils.write(pageUtilsNEW)
     pageUtils.close()
 end
+if mite then
+    miteContent = mite.readAll()
+else
+    print("Mite not found, creating new one...")
+    mite = fs.open("/web/mite.lua", "w")
+    mite.write(miteNEW)
+    mite.close()
+end
 
 if webNEW == webContent then
     print("Web is up to date!")
@@ -56,5 +66,14 @@ else
     print("PageUtils is not up to date!")
     local file = fs.open("/web/pageUtils.lua", "w")
     file.write(pageUtilsNEW)
+    file.close()
+end
+
+if miteNEW == miteContent then
+    print("Mite is up to date!")
+else
+    print("Mite is not up to date!")
+    local file = fs.open("/web/mite.lua", "w")
+    file.write(miteNEW)
     file.close()
 end
